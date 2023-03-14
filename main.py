14af5d93dceae70990f2adf44c66303cad2f260d
Chandravo <chandravob2003@gmail.com>
2023-03-14 17:34:15 +0530

    fixed last and numq scrappeer

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 
 def datatransform():
     n = 1
-    username = ["rimjhimittal", 'chandravob', 'askandola', 'tijilm', 'mhardik003_', 'shreeyachatzz', 'yashmittal', 'gunjeevsingh', 'anshbajaj07', 'happy2901', 'aitchessbee', 'samikm', 'rdotjain']
+    username = ["rimjhimittal", 'chandravob', 'askandola', 'tijilm', 'mhardik003_', 'shreeyachatzz', 'yashmittal', 'gunjeevsingh', 'anshbajaj07', 'happy2901', 'aitchessbee', 'samikm']
+    # username = ["rimjhimittal", 'chandravob']
     df = pd.DataFrame(columns=['Name', 'Username', 'Rank', 'Photo', 'Last Solved', 'Number of Questions'])
     for k in username:
         url = "https://leetcode.com/" + k
@@ -12,8 +13,8 @@ def datatransform():
         htmlContent = r.content
         soup = BeautifulSoup(htmlContent, 'html.parser')
         rank = soup.find("span", class_="ttext-label-1 dark:text-dark-label-1 font-medium") .get_text()
-        last = soup.find("span", class_="text-label-3 dark:text-dark-label-3 hidden whitespace-nowrap lc-md:inline")
-        numq = soup.find("div", class_="text-[24px] font-medium text-label-1 dark:text-dark-label-1")
+        last = soup.find("span", class_="text-label-3 dark:text-dark-label-3 hidden whitespace-nowrap lc-md:inline").text
+        numq = soup.find("div", class_="text-[24px] font-medium text-label-1 dark:text-dark-label-1").text
         name = soup.find("div", class_="text-label-1 dark:text-dark-label-1 break-all text-base font-semibold").get_text()
         images = soup.findAll('img', class_ = "h-20 w-20 rounded-lg object-cover")
         example = images[0]
@@ -24,7 +25,7 @@ def datatransform():
         #     print(text)
         # print(soup.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div[3]/div/div/div[2]/a[1]/div/span[2]"))
 
-
+        print(name," done")
         rank2 = ""
         for i in rank:
             if i.isnumeric():
@@ -38,8 +39,11 @@ def datatransform():
         df.loc[n, 'Name'] = name
         df.loc[n, 'Number of Questions'] = numq
         n=n+1
+        
     a = df.sort_values(by='Rank')
     print(a)
     b = a.to_csv('details.csv', index=False)
     return a
+
+datatransform()
 
