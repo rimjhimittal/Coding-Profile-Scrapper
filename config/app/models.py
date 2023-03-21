6e9908218a1bb2490488046ec0470aa400f4ad2f
Chandravo <chandravob2003@gmail.com>
2023-03-21 14:43:38 +0530

    backend init

---
+++ config/app/models.py
@@ -0,0 +1,46 @@
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
+from django.contrib.auth.models import PermissionsMixin
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    leetcode_name = models.CharField(max_length=100, null=True, blank=True, default=None)    
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    otp = models.CharField(max_length=7, null=True, blank=True, default=None)
+    
+    def get_short_name(self):
+        # The user is identified by their email
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+    
+    def has_module_perms(self, app_label):
+           return True
+    
+    def __str__(self):
+        return self.email
+    
+
+class leetcode_details(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True, blank=True, default=None)
+    username = models.CharField(max_length=100, null=True, blank=True, default=None)
+    rank= models.CharField(max_length=100, null=True, blank=True, default=None)
+    photo_url = models.CharField(max_length=100, null=True, blank=True, default=None)
+    number_of_questions = models.IntegerField(null=True, blank=True, default=None)
+    last_solved = models.CharField(max_length=100, null=True, blank=True, default=None)
+    
+    def __str__(self):
+        return self.username
